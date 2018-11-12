40ce0a49c9acb120b85e8a2cad02b733f836bcc5
elegidoadedo <jmoralesmnz@gmail.com>
2018-11-12 14:24:55 +0100

    edit profiles done

--- routes/profile.js
+++ routes/profile.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
+const ObjectId = mongoose.Types.ObjectId;
 const User = require('../models/user');
 
 
 
-router.patch('/edit', (req, res) => {
+router.put('/edit', (req, res, next) => {
   const data = req.body
   const user = req.session.currentUser._id;
   User.findByIdAndUpdate(user, data)
+ 
   .then((result)=>{
-    res.status(200).json(result)
+    console.log(data)
+    res.status(200).json(data)
   })
   .catch(next)
 });
@@ -43,48 +44,18 @@ router.patch('/addContact', (req, res) => {
 });
 
 
-// const id = req.session.currentUser._id; 
-// const {code} = req.body
-// Client.findById(id)
-// .then(client => {
-//   Professional.findOne({code})
-//   .then(professional => {
-//     if(!professional){
-//       req.flash('error', 'El profesional no existe')
-//       return res.redirect('/clients/add-favorite')
-//     }
-//     professionalId = professional._id
 
-//     client.myProfessionals.forEach(item => {
-//       if(professionalId.toString() === item._id.toString()){
-//         req.flash('error', 'Ya tienes añadido este profesional')
-//         return res.redirect('/clients/add-favorite')
-//       }
-//     })
-
-//     client.myProfessionals.push(ObjectId(professionalId))
-//     client.save()
-//     .then(succes => {
-//       req.flash('info', 'Añadido correctamente');
-//       return res.redirect('/clients/my-favorites');
-//     })
-//     .catch(next)
-//   })
-//   .catch(next)
-// })
-// .catch(next)
-// })
-
-// router.get('/find', (req, res) => {
-//   const userId = req.session.currentUser._id;
-//   User.findById(userId)
-//   .then((result)=>{
-//     console.log(result)
-//     res.status(200).json(result)
+router.get('/info', (req, res, next) => {
+  const userId = req.session.currentUser._id;
+  User.findById(userId)
+  .populate('contacts')
+  .then((result)=>{
+    console.log(result)
+    res.status(200).json(result)
     
-//   })
-//   .catch(next)
-// });
+  })
+  .catch(next)
+});
 
 
 
